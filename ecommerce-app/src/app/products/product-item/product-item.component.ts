2c25b875e70754e22a5672941a26ce1f45b58916
Demian-1 <sergio_demian_ae@hotmail.com>
2024-07-01 14:56:42 -0600

    added change qty to product item in cart functionality

--- ecommerce-app/src/app/products/product-item/product-item.component.ts
+++ ecommerce-app/src/app/products/product-item/product-item.component.ts
@@ -1,16 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { CardModule } from 'primeng/card';
-import { Button } from 'primeng/button';
+import { ButtonModule } from 'primeng/button';
 import { Product } from '../../model/Product';
 import { AuthService } from '../../service/auth.service';
 import { ShoppingCartService } from '../../service/shoppingcart.service';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 @Component({
   selector: 'app-product-item',
   standalone: true,
   imports: [
     CardModule,
-    Button
+    ButtonModule,
+    InputNumberModule,
+    FormsModule
   ],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.css'
@@ -18,10 +22,17 @@ import { ShoppingCartService } from '../../service/shoppingcart.service';
 export class ProductItemComponent {  
   @Input() product = new Product();
   userId : number | null = null;
+  qty = 1;
   constructor(
     private authService: AuthService,
     private shoppingCartService: ShoppingCartService,
   ) { }
+
+  substractQty(){
+    if(this.qty>1){
+      this.qty = this.qty - 1;
+    }
+  }
 
   ngOnInit(){
     this.userId = this.authService.getUserId();
@@ -29,7 +40,7 @@ export class ProductItemComponent {
 
   addToCart(idProducto: number){
     if(this.userId){
-      this.shoppingCartService.addProductToCart(idProducto, this.userId, 1).subscribe(
+      this.shoppingCartService.addProductToCart(idProducto, this.userId, this.qty).subscribe(
         () => {
           alert("Agregado con exito");
         }
