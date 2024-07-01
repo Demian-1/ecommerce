765c2acc17fb8a99f15c3b0b5f20739f337bf1a2
pigchip <guzmancruzandresmiguel@gmail.com>
2024-07-01 00:36:45 -0600

    Ajustes finales del Login

--- ecommerce-app/src/app/header/header.component.ts
+++ ecommerce-app/src/app/header/header.component.ts
@@ -2,28 +2,48 @@ import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 import { TabMenuModule } from 'primeng/tabmenu';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { Router } from '@angular/router';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [TabMenuModule, RouterLink],
+  imports: [TabMenuModule, RouterLink, ButtonModule, DialogModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
   elementosDelMenu: MenuItem[] | undefined;
+  displayLogoutDialog: boolean = false;
+
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
     this.elementosDelMenu = [
-      {label: 'Home', icon: 'pi pi-home', routerLink: '/home'},
-      {label: 'Categorías', icon: 'pi pi-tags', routerLink: '/categoria'},
-      {label: 'Productos', icon: 'pi pi-box', routerLink: '/product'},
-      {label: 'Carrito', icon: 'pi pi-shopping-cart', routerLink: '/shopping_cart'},
-      {label: 'Orden', icon: 'pi pi-file', routerLink: '/shop_order'},
-      {label: 'Método de Pago', icon: 'pi pi-wallet', routerLink: '/user_payment_method'},
-      {label: 'Dirección de Usuario', icon: 'pi pi-map', routerLink: '/user_address'},
-      {label: 'Registro', icon: 'pi pi-user-edit', routerLink: '/registroForm'},
-      {label: 'Login', icon: 'pi pi-user-plus', routerLink: '/loginForm'}
+      { label: 'Home', icon: 'pi pi-home', routerLink: '/home' },
+      { label: 'Categorías', icon: 'pi pi-tags', routerLink: '/categoria' },
+      { label: 'Productos', icon: 'pi pi-box', routerLink: '/product' },
+      { label: 'Carrito', icon: 'pi pi-shopping-cart', routerLink: '/shopping_cart' },
+      { label: 'Orden', icon: 'pi pi-file', routerLink: '/shop_order' },
+      { label: 'Método de Pago', icon: 'pi pi-wallet', routerLink: '/user_payment_method' },
+      { label: 'Dirección de Usuario', icon: 'pi pi-map', routerLink: '/user_address' },
+      { label: 'Logout', icon: 'pi pi-sign-out', command: () => this.showLogoutDialog() }
     ];
+  }
+
+  showLogoutDialog() {
+    this.displayLogoutDialog = true;
+  }
+
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/loginForm']);
+    this.displayLogoutDialog = false;
+  }
+
+  onCancel() {
+    this.displayLogoutDialog = false;
   }
 }
