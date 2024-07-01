6f6bb861e33403023e0e8bd33c9259a3de69cc77
pigchip <guzmancruzandresmiguel@gmail.com>
2024-06-30 22:38:48 -0600

    Rutas

--- ecommerce-app/src/app/app.component.ts
+++ ecommerce-app/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardModule } from 'primeng/card';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ ecommerce-app/src/app/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent {}
