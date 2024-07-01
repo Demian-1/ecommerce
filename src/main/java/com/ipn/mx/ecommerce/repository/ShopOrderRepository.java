1fd4eff9cbca5a107f86a64f473499c57475c110
Diego Zalazar <dzm321890@gmail.com>
2024-07-01 09:32:26 -0600

    delete shopOrder

--- src/main/java/com/ipn/mx/ecommerce/repository/ShopOrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ipn.mx.ecommerce.repository;
-
-import com.ipn.mx.ecommerce.model.ShopOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShopOrderRepository extends JpaRepository<ShopOrder, Integer> {
-}
